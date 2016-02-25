1f5cbb7a379a1597544e382ae54f2d66d104da12
pirriadoc <butcher198038@gmail.com>
2016-02-25 21:30:47 +0100

    ldap

--- settings.py
+++ settings.py
@@ -17,6 +17,7 @@ INSTALLED_ADDONS = [
     'aldryn-newsblog',
     'aldryn-people',
     'aldryn-style',
+#   'django_extensions',
     # </INSTALLED_ADDONS>
 ]
 
@@ -27,7 +28,8 @@ aldryn_addons.settings.load(locals())
 # all django settings can be altered here
 
 INSTALLED_APPS.extend([
-    # add you project specific apps here
+    # add you project specific apps he
+    'django_extensions',
 ])
 
 TEMPLATE_CONTEXT_PROCESSORS.extend([
@@ -37,3 +39,38 @@ TEMPLATE_CONTEXT_PROCESSORS.extend([
 MIDDLEWARE_CLASSES.extend([
     # add your own middlewares here
 ])
+
+import ldap
+from django_auth_ldap.config import LDAPSearch, PosixGroupType
+
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+AUTH_LDAP_SERVER_URI = "ldap://172.17.0.1"
+
+AUTH_LDAP_BIND_DN = "cn=admin,dc=example,dc=org"
+AUTH_LDAP_BIND_PASSWORD = "admin"
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=People,dc=example,dc=org",
+    ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("ou=Groups,dc=example,dc=org",
+    ldap.SCOPE_SUBTREE, "(objectClass=PosixGroup)"
+)
+AUTH_LDAP_GROUP_TYPE = PosixGroupType(name_attr="cn")
+
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    "is_active": "cn=active,ou=Groups,dc=example,dc=org",
+    "is_staff": "cn=staff,ou=groups,dc=example,dc=org",
+    "is_superuser": "cn=superuser,ou=Groups,dc=example,dc=org"
+}
+
+AUTH_LDAP_FIND_GROUP_PERMS = True
+
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn"
+}
+
+CMS_PERMISSION = True
